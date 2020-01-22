0dfd007627d349d792075b2169e96520462dd1f5
ParkSeongJun <ParkSeongJun@192.168.0.9>
2020-01-23 01:00:47 +0900

    1주차/PSJ 수정

--- PSJ/Alps0130/Code1284.java
+++ PSJ/Alps0130/Code1284.java
@@ -8,54 +8,26 @@ public class Code1284 {
 	public static void main(String[] args) {
 
 		Scanner scan = new Scanner(System.in);
-
 		ArrayList<Integer> primeNum = new ArrayList<>(); // 가변적인 ArrayList 사용
-
-		int num2, startNum = 2;
-		int primeNum1 = 0, primeNum2 = 0; // 결과값
-		boolean prime;
 
 		System.out.print("숫자 한 개를 입력하시오 : ");
 
 		int num1 = scan.nextInt();
 
-		// 입력받은 값까지의 소수들을 ArrayList에 저장
-		while (startNum <= num1) {
-			prime = true;
-
-			for (int i = 2; i < startNum; i++) {
-				if (startNum % i == 0) {
-					prime = false;
-					break;
+		while (true) {
+			for (int i = 2; i <= num1; i++) {
+				if (num1 % i == 0) {
+					primeNum.add(i);
+					num1 /= i;
 				}
 			}
-
-			if (prime == true)
-				primeNum.add(startNum);
-			startNum++;
+			
+			if (num1 == 1)
+				break;
 		}
-
-		prime = false; // 초기화
-
-		for (int i = 0; i < primeNum.size(); i++) {
-			if (num1 % primeNum.get(i) == 0) {
-				num2 = num1 / primeNum.get(i);
-				for (int j = i; j < primeNum.size(); j++) {
-					if (primeNum.get(j) == num2) {
-						primeNum1 = primeNum.get(i);
-						primeNum2 = num2;
-						prime = true;
-						break;
-					}
-				}
-
-				if (prime == true)
-					break;
-			}
-		}
-
-		if (prime == true)
-			System.out.println(primeNum1 + " " + primeNum2);
+		
+		if (primeNum.size() == 2)
+			System.out.println(primeNum.get(0) + " " + primeNum.get(1));
 		else
 			System.out.println("wrong number");
 	}
