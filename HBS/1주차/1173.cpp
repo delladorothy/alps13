c257e9ada552b79452cb8ab8fadb7cdc107f2813
범석 한 <bmtoss@naver.com>
2020-01-24 12:28:36 +0900

    1173

---
+++ HBS/1주차/1173.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main(void) 
+{
+	int time=24;
+	int minute=60;
+
+	cout << "time < 24 &&minute < 60" << endl;
+	while (time >= 24 && minute >= 60)								//time,minute이 범위 밖의 값을 입력시 반복
+	{
+		cin >> time;
+		cin >> minute;
+		if (time >= 24 && minute >= 60) 
+		{
+			cout << "time < 24 &&minute < 60" << endl;
+			cout << "다시입력: ";
+		}
+	}
+	if (minute < 30)												//minute이 30보다작을경우 시간값이 바뀐다.
+	{
+		if (time == 0)												//time이 0일경우 23으로 바뀐다.
+		{
+			time = 23;
+		}
+		else 
+		{
+			time -= 1;
+			minute = 60 - (30 - minute);
+		}
+	}
+	else 
+	{
+		minute -= 30;
+	}
+
+	cout << time << " " << minute << endl;
+}
