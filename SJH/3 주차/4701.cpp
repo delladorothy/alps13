013d75c0d44926a9b491bee4a7a45184ffb6d93f
dpsjh <60134780+dpsjh@users.noreply.github.com>
2020-04-02 16:36:48 +0900

    Update 4701.cpp

--- SJH/3 주차/4701.cpp
+++ SJH/3 주차/4701.cpp
@@ -1,36 +1,38 @@
 #include<iostream>
 #include<vector>
-#include<tuple>
 #include<algorithm>
 using namespace std;
 
 int main() {
 	int num;
 	int a;
+	int k = 0;
 	vector<int> array1;
-	vector<tuple<int, int, int>>array2;
-
 	cin >> num;
 
 	for (int i = 0; i < num; i++) {
 		cin >> a ;
 		array1.push_back(a);
 	}
-	
+	int b = array1.at(0);
+	int c = array1.at(1);
+	k = abs(array1.at(0) + array1.at(1));
 	for (int i = 0; i < num; i++) {
 		for (int j = i + 1; j < num; j++) {
-			array2.push_back(tuple<int, int, int>(abs(array1.at(i)+array1.at(j)),  array1.at(i), array1.at(j)));
-
+			int a = abs(array1.at(i) + array1.at(j));
+			if (k > a) {
+				b = array1.at(i);
+				c = array1.at(j);
+				k = a;
+			}
 		}
 	}
 
-	sort(array2.begin(), array2.end());
-
-	if (get<1>(array2[0]) < get<2>(array2[0])) {
-		cout << get<1>(array2[0]) << " " << get<2>(array2[0]) << endl;
+	if (b < c) {
+		cout <<b << " " << c << endl;
 	}
 	else {
-		cout << get<2>(array2[0]) << " " << get<1>(array2[0]) << endl;
+		cout << c << " " <<b << endl;
 	}
 
 	return 0;
